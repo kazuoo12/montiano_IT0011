8be9cd41214aeee9388683e91ddf324e31dbcfb3
kazuoo12 <clrm0943@gmail.com>
2025-01-23 11:30:48 +0800

    Technical1

---
+++ Technical 1.py
@@ -0,0 +1,24 @@
+name1 = input("Enter First Name: ")
+name2 = input("Enter Last Name: ")
+name3 = input("Enter Middle Name: ")
+
+birthday = input("Enter Birthday: ")
+
+gender = input("Enter Gender: ")
+
+address = input("Enter your Address: ")
+
+course = input("Enter your Course: ")
+
+yearlevel = input("What is your Year Level: ")
+
+print("============================================")
+
+print("First name: ", name1)
+print("Last name: ", name2)
+print("Middle name: ", name3)
+print("Birthday: ", birthday)
+print("Gender: ", gender)
+print("Address: ", address)
+print("Course: ", course)
+print("Year Level: ", yearlevel)
